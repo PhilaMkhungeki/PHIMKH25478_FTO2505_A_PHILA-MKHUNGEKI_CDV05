bd6b33ea9d8600d1e5abe81fd1d5b37f4f5ade18
Phila <mkhungekiphila@gmail.com>
2025-09-16 23:54:34 +0200

    Update small screen responsiveness when scrolling

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
- const sections = document.querySelectorAll('section');
-
- const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting) {
-            entry.target.classList.add('active');
-        } else {
-            entry.target.classList.remove('active');
-        }
-    });
- }, {threshold: 0.5});
-
- sections.forEach(section => {
-    observer.observe(section);
- }); 
+ 
 
  //make the menu icon clickable to show menu items
 const menu = document.getElementById("menu");
